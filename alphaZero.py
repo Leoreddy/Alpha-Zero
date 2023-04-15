931626f3c8a4993e2f90ca05347c4a8e147c464c
Leo R <77698534+Leoreddy@users.noreply.github.com>
2023-04-15 15:42:40 +0200

    Update alphaZero.py
    Completed the code for TicTacToe class

--- alphaZero.py
+++ alphaZero.py
@@ -4,10 +4,10 @@ class TicTacToe:
     def __init__(self):
         self.row_count = 3
         self.column_count = 3
-        self.action_size = self.row_count+self.column_count
-
-    def get_inital_state(self):
-        return np.zeros(self.row_count, self.column_count)
+        self.action_size = self.row_count * self.column_count
+        
+    def get_initial_state(self):
+        return np.zeros((self.row_count, self.column_count))
     
     def get_next_state(self, state, action, player):
         row = action // self.column_count
@@ -22,19 +22,51 @@ class TicTacToe:
         row = action // self.column_count
         column = action % self.column_count
         player = state[row, column]
-
-        return{
-            np.sum(state[rowm, :]) == player + self.column_count
-            or np.sum(state[:, column]) == player + self.row_count
-            or (np.sum.np.diag(state)) == player+self.row_count
-            or np.sum(np.diag(np.flip(state, axis = 0))) == player + self.row_count
-        }
+        
+        return (
+            np.sum(state[row, :]) == player * self.column_count
+            or np.sum(state[:, column]) == player * self.row_count
+            or np.sum(np.diag(state)) == player * self.row_count
+            or np.sum(np.diag(np.flip(state, axis=0))) == player * self.row_count
+        )
     
     def get_value_and_terminated(self, state, action):
         if self.check_win(state, action):
-            return l, True
+            return 1, True
         if np.sum(self.get_valid_moves(state)) == 0:
-            return True
-        
+            return 0, True
+        return 0, False
+    
     def get_opponent(self, player):
         return -player
+
+
+tictactoe = TicTacToe()
+player = 1
+
+state = tictactoe.get_initial_state()
+
+
+while True:
+    print(state)
+    valid_moves = tictactoe.get_valid_moves(state)
+    print("valid_moves", [i for i in range(tictactoe.action_size) if valid_moves[i] == 1])
+    action = int(input(f"{player}:"))
+    
+    if valid_moves[action] == 0:
+        print("action not valid")
+        continue
+        
+    state = tictactoe.get_next_state(state, action, player)
+    
+    value, is_terminal = tictactoe.get_value_and_terminated(state, action)
+    
+    if is_terminal:
+        print(state)
+        if value == 1:
+            print(player, "won")
+        else:
+            print("draw")
+        break
+        
+    player = tictactoe.get_opponent(player)
